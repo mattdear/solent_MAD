2841cb2654e268f568caa5b4197d9435aea0a34c
Matthew Dear <0dearm11@solent.ac.uk>
2020-01-28 12:51:52 +0000

    I have started session 3 but, this now needs to be completed.

--- AAA_MyClassWork/week3/Mapping/app/src/main/java/com/example/mapping/MainActivity.java
+++ AAA_MyClassWork/week3/Mapping/app/src/main/java/com/example/mapping/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.mapping;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    MapView mv;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        // This line sets the user agent, a requirement to download OSM maps
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
+        setContentView(R.layout.activity_main);
+
+        mv = (MapView) findViewById(R.id.map1);
+
+        mv.setBuiltInZoomControls(true);
+        mv.getController().setZoom(17);
+        mv.getController().setCenter(new GeoPoint(50.9349, -1.4219));
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onClick(View v) {
+
     }
 }
