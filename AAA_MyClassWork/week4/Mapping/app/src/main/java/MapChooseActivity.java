bdae8f25d954269de2f212f105dab5c1535ab11a
Matthew Dear <0dearm11@solent.ac.uk>
2020-02-04 12:34:41 +0000

    started session 4 and session 5

---
+++ AAA_MyClassWork/week4/Mapping/app/src/main/java/MapChooseActivity.java
@@ -0,0 +1,16 @@
+public class MapChooseActivity {
+
+    import android.support.v7.app.AppCompatActivity;
+    import android.os.Bundle;
+    import android.content.Intent;
+    import android.widget.Button;
+    import android.view.View;
+
+    public class MapChooseActivity extends AppCompatActivity {
+
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_map_choose);
+        }
+    }
+}
