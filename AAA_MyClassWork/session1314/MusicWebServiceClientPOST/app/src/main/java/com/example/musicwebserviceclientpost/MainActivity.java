d4833917093ab913604ec200b2397fedbe7a5d6c
Matt Dear <mrmdear@gmail.com>
2020-03-03 19:06:10 +0000

    I think I have now completed task3 but this is going to need to be tested on my laptop.

--- AAA_MyClassWork/session1314/MusicWebServiceClientPOST/app/src/main/java/com/example/musicwebserviceclientpost/MainActivity.java
+++ AAA_MyClassWork/session1314/MusicWebServiceClientPOST/app/src/main/java/com/example/musicwebserviceclientpost/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.musicwebserviceclientpost;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    class MyTask extends AsyncTask<String, Void, String> {
+        public String doInBackground(String... userInput) {
+            HttpURLConnection conn = null;
+            try {
+                URL url = new URL("http://www.free-map.org.uk/course/ws/addhit.php");
+                conn = (HttpURLConnection) url.openConnection();
+
+                String postData = "song=" + userInput[0] + "&artist=" + userInput[1] + "&year=" + userInput[2];
+                // For POST
+                conn.setDoOutput(true);
+                conn.setFixedLengthStreamingMode(postData.length());
+
+                OutputStream out = null;
+                out = conn.getOutputStream();
+                out.write(postData.getBytes());
+                if (conn.getResponseCode() == 200) {
+                    InputStream in = conn.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                    String all = "", line;
+                    while ((line = br.readLine()) != null)
+                        all += line;
+                    return all;
+                } else {
+                    return "HTTP ERROR: " + conn.getResponseCode();
+                }
+            } catch (IOException e) {
+                return e.toString();
+            } finally {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            }
+        }
+
+        public void onPostExecute(String result) {
+
+            new AlertDialog.Builder(MainActivity.this).
+                    setMessage("Server sent back: " + result).
+                    setPositiveButton("OK", null).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button search = (Button) findViewById(R.id.searchbtn);
+        search.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        EditText et1 = (EditText) findViewById(R.id.et1);
+        String song = et1.getText().toString();
+        EditText et2 = (EditText) findViewById(R.id.et2);
+        String artist = et2.getText().toString();
+        EditText et3 = (EditText) findViewById(R.id.et3);
+        String year = et3.getText().toString();
+
+        MyTask t = new MyTask();
+        t.execute(song, artist, year);
     }
 }
