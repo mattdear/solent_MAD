d7c17fa75957cde5a580188e82aabf1018120bdc
Matthew Dear <0dearm11@solent.ac.uk>
2020-03-03 12:19:54 +0000

    I have now completed task 1 and 2 and can now do a GET request from HitTastic.

--- AAA_MyClassWork/session1314/MusicWebServiceClient/app/src/main/java/com/example/musicwebserviceclient/MainActivity.java
+++ AAA_MyClassWork/session1314/MusicWebServiceClient/app/src/main/java/com/example/musicwebserviceclient/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.musicwebserviceclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    class MyTask extends AsyncTask<String, Void, String> {
+        public String doInBackground(String... userInput) {
+            HttpURLConnection conn = null;
+            try {
+                URL url = new URL("http://www.free-map.org.uk/course/ws/hits.php?artist=" + userInput[0]);
+                conn = (HttpURLConnection) url.openConnection();
+                InputStream in = conn.getInputStream();
+                if (conn.getResponseCode() == 200) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                    String result = "", line;
+                    while ((line = br.readLine()) != null) {
+                        result += line;
+                    }
+                    return result;
+                } else {
+                    return "HTTP ERROR: " + conn.getResponseCode();
+                }
+            } catch (IOException e) {
+                return e.toString();
+            } finally {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            }
+        }
+
+        public void onPostExecute(String result) {
+            TextView tv1 = (TextView) findViewById(R.id.tv1);
+            tv1.setText(result);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button search = (Button) findViewById(R.id.searchbtn);
+        search.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        EditText et = (EditText) findViewById(R.id.et1);
+        String userInput = et.getText().toString();
+        MyTask t = new MyTask();
+        t.execute(userInput);
     }
 }
