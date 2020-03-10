d93c837248afb6358804ca1e8403a26f4d8220b1
Matthew Dear <0dearm11@solent.ac.uk>
2020-03-10 12:29:09 +0000

    I think I have finished this weeks task just need to test it on the laptop.

--- AAA_MyClassWork/session1516/MusicWebServiceClient/app/src/main/java/com/example/musicwebserviceclient/MainActivity.java
+++ AAA_MyClassWork/session1516/MusicWebServiceClient/app/src/main/java/com/example/musicwebserviceclient/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.musicwebserviceclient;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
@@ -8,6 +9,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public String doInBackground(String... userInput) {
             HttpURLConnection conn = null;
             try {
-                URL url = new URL("http://www.free-map.org.uk/course/ws/hits.php?artist=" + userInput[0]);
+                URL url = new URL("https://hikar.org/course/ws/hits.php?artist=" + userInput[0] + "&format=json");
                 conn = (HttpURLConnection) url.openConnection();
                 InputStream in = conn.getInputStream();
                 if (conn.getResponseCode() == 200) {
@@ -45,8 +50,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         public void onPostExecute(String result) {
-            TextView tv1 = (TextView) findViewById(R.id.tv1);
-            tv1.setText(result);
+            try {
+                JSONArray jsonArr = new JSONArray(result);
+
+                String text = "";
+
+                for (int i = 0; i < jsonArr.length(); i++) {
+                    JSONObject curObj = jsonArr.getJSONObject(i);
+                    String ID = curObj.getString("ID"),
+                            title = curObj.getString("title"),
+                            artist = curObj.getString("artist"),
+                            year = curObj.getString("year");
+                    text += " ID= " + ID + " Title= " + title + " Artist= " + artist + " Year= " + year + "\n";
+                }
+
+                TextView tv1 = (TextView) findViewById(R.id.tv1);
+                tv1.setText(text);
+
+            } catch (JSONException e) {
+                TextView tv1 = (TextView) findViewById(R.id.tv1);
+                tv1.setText(e.toString());
+            }
         }
     }
 
